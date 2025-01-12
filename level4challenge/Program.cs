8da40b20676a04727e88e14fe1e9af1384697aef
ryan <ryankaneohe@gmail.com>
2025-01-12 09:53:39 -0500

    First commit

---
+++ level4challenge/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// ask user for object //
+Console.WriteLine("What kind of thing are we talking about?");
+// TODO: rename these strings with more descriptive names //
+string a = Console.ReadLine();
+/* ask user for description of object */
+Console.WriteLine("How would you describe it? Big? Azure? Tattered?");
+string b = Console.ReadLine();
+string c = " of Doom";
+string d = "3000"; 
+Console.WriteLine("The " + b + " " + a + c + " " + d + "!");
+
---
+++ level_3/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// This is a comment
+/* And this is a comment too */
+
+Console.WriteLine("Hello, Ritlin!");
+Console.WriteLine("I may have only just met you, but I love you.");
+Console.WriteLine("Tell me more about the Uncoded One.");
+Console.WriteLine();
+Console.WriteLine("Bread is ready.");
+Console. WriteLine("Who is the bread for?");
+string name;
+name = Console.ReadLine();
+Console.WriteLine("Noted: " + name + " received bread.");
