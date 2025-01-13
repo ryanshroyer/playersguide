91412048e77b62e0a47a2688b8220291038cf069
ryan <ryankaneohe@gmail.com>
2025-01-12 21:34:11 -0500

    Completed Defense of Consolas

---
+++ level8/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Enter the column number:");
+short column = Convert.ToInt16(Console.ReadLine());
+Console.WriteLine("Enter the row number:");
+short row = Convert.ToInt16(Console.ReadLine());
+
+Console.BackgroundColor = ConsoleColor.Yellow;
+Console.ForegroundColor = ConsoleColor.Black;
+
+Console.Clear();
+Console.Title = "Defense of Consolas";
+
+Console.WriteLine("Deploy to: (Read Column, Row)");
+// Console.Beep(440, 333); // Beep method only works on Windows
+Console.WriteLine($"({column}, {row + 1})");
+Console.WriteLine($"({column + 1}, {row})");
+Console.WriteLine($"({column}, {row - 1})");
+Console.WriteLine($"({column - 1}, {row})");
